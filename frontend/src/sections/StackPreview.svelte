6aeae69744d6a27135aac7dafb2121564b72a0ec
kts of kettek <kts@kettek.net>
2024-05-23 00:28:18 -0700

    Add horrid preview adjustment for groups
    need to rework the stack preview

--- frontend/src/sections/StackPreview.svelte
+++ frontend/src/sections/StackPreview.svelte
@@ -4,15 +4,25 @@
   This component provides a sprite stack view of a file.
 -->
 <script lang='ts'>
-  import { Grid, Row, Column, Checkbox, Slider } from "carbon-components-svelte"
+  import { Grid, Row, Column, Checkbox, Slider, Dropdown } from "carbon-components-svelte"
   import { fileStates } from "../stores/file"
   import { onMount } from "svelte";
   
-  let shownFiles: Record<string, boolean> = {}
+  type GroupState = {
+    visible: boolean
+    animation: string
+    frameIndex: number
+  }
+  type VisibleState = {
+    visible: boolean
+    groups: Record<string, GroupState>
+  }
+  let visibleFiles: Record<string, VisibleState> = {}
+  
   let filePositions: Record<string, {x: number, y: number, z: number}> = {}
   $: {
     for (let file of $fileStates) {
-      if (shownFiles[file.title] && !filePositions[file.title]) {
+      if (visibleFiles[file.title] && !filePositions[file.title]) {
         filePositions[file.title] = {x: 0, y: 0, z: 0}
       }
     }
@@ -50,12 +60,13 @@
     
     let x = canvas.width/2
     let y = canvas.height/2
-    let sortedFiles = $fileStates.filter(file => shownFiles[file.title]).sort((a, b) => filePositions[a.title].z - filePositions[b.title].z)
+    let sortedFiles = $fileStates.filter(file => visibleFiles[file.title]).sort((a, b) => filePositions[a.title].z - filePositions[b.title].z)
     for (let file of sortedFiles) {
-      if (shownFiles[file.title]) {
+      if (visibleFiles[file.title]?.visible) {
         // For now, just get the first frame of the first animation.
-        let done = false
         for (let group of file.groups) {
+          let done = false
+          if (!visibleFiles[file.title]?.groups[group.name]?.visible) continue
           for (let animation of group.animations) {
             for (let frame of animation.frames) {
               for (let sliceIndex = 0; sliceIndex < frame.slices.length; sliceIndex++) {
@@ -105,7 +116,6 @@
             if (done) break
           }
           y = canvas.height/2
-          if (done) break
         }
       }
     }
@@ -116,7 +126,7 @@
     y *= zoom
     for (let file of $fileStates) {
       let name = file.title
-      if (!shownFiles[name]) continue
+      if (!visibleFiles[name]) continue
       let px = canvas.width/2
       let py = canvas.height/2
       px += filePositions[name].x
@@ -153,6 +163,42 @@
     window.addEventListener('mousemove', mousemove)
   }
   
+  function toggleFile(file, i, e) {
+    visibleFiles[file.title] = visibleFiles[file.title] || {visible: e.target.checked, groups: {}}
+    visibleFiles[file.title].visible = e.target.checked
+    for (let group of file.groups) {
+      visibleFiles[file.title].groups[group.name] = visibleFiles[file.title].groups[group.name] || {visible: e.target.checked, animation: group.animations[0]?.name, frameIndex: 0}
+      visibleFiles[file.title].groups[group.name].visible = e.target.checked
+    }
+    visibleFiles = {...visibleFiles}
+  }
+  
+  function toggleGroup(file, group, e) {
+    if (!visibleFiles[file.title]) visibleFiles[file.title] = {visible: true, groups: {}}
+    visibleFiles[file.title].groups[group.name] = visibleFiles[file.title].groups[group.name] || {visible: e.target.checked, animation: group.animations[0]?.name, frameIndex: 0}
+    visibleFiles[file.title].groups[group.name].visible = e.target.checked
+    visibleFiles = {...visibleFiles}
+  }
+  
+  function isFileIndeterminate(file) {
+    let visible = visibleFiles[file.title]?.visible
+    let groupCount = 0
+    let visibleGroupCount = 0
+    for (let group of file.groups) {
+      groupCount++
+      if (visibleFiles[file.title]?.groups[group.name]) {
+        visibleGroupCount++
+      }
+    }
+    return (visible && visibleGroupCount !== groupCount) || (!visible && visibleGroupCount === 0)
+  }
+  function changeGroupAnimation(file, group, e) {
+    if (!visibleFiles[file.title]) visibleFiles[file.title] = {visible: true, groups: {}}
+    visibleFiles[file.title].groups[group.name] = visibleFiles[file.title].groups[group.name] || {visible: true, animation: group.animations[0]?.name, frameIndex: 0}
+    visibleFiles[file.title].groups[group.name].animation = e.detail.selectedId
+    visibleFiles = {...visibleFiles}
+  }
+  
   onMount(()=>{
     let frameID: number = 0
     let frameDraw = () => {
@@ -168,7 +214,13 @@
   <Row narrow condensed>
     <Column sm>
       {#each $fileStates as file, i}
-        <Checkbox bind:checked={shownFiles[file.title]} labelText={file.title.length>20?'…'+file.title.substring(file.title.length-20):file.title}></Checkbox>
+        <Checkbox on:change={(e)=>toggleFile(file, i, e)} checked={visibleFiles[file.title]?.visible} indeterminate={isFileIndeterminate(file)} labelText={file.title.length>20?'…'+file.title.substring(file.title.length-20):file.title}></Checkbox>
+        {#each file.groups as group, groupIndex}
+          <div class='subcheck'>
+            <Checkbox on:change={(e)=>toggleGroup(file, group, e)} checked={visibleFiles[file.title]?.groups[group.name]?.visible} labelText={group.name.length>20?'…'+group.name.substring(group.name.length-20):group.name}></Checkbox>
+            <Dropdown on:select={(e)=>changeGroupAnimation(file, group, e)} selectedId={visibleFiles[file.title]?.groups[group.name]?.animation} items={group.animations.map(animation => ({id: animation.name, text: animation.name}))}></Dropdown>
+          </div>
+        {/each}
       {/each}
     </Column>
     <Column>
@@ -182,4 +234,7 @@
 </Grid>
 
 <style>
+  .subcheck {
+    margin-left: 1em;
+  }
 </style>
