f7f66640271780ce198d329d33578a0e0b4af8e2
kettek <kts@kettek.net>
2024-06-19 15:49:58 -0700

    Add rename modal

---
+++ frontend/src/components/RenameModal.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+  import { Modal, TextInput } from "carbon-components-svelte"
+
+  export let open: boolean = false
+  export let heading: string = "Rename"
+  export let onsubmit: (v: string) => void = () => {}
+
+  export let text: string = ""
+
+</script>
+
+<Modal
+  bind:open
+  modalHeading={heading}
+  primaryButtonText="Apply"
+  secondaryButtonText="Cancel"
+  on:close={() => open = false}
+  on:click:button--secondary={() => open = false}
+  on:submit={() => {
+    open = false
+    onsubmit(text)
+  }}
+>
+  <TextInput id="rename" labelText="New name" bind:value={text} />
+</Modal>
