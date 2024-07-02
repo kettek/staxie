21411e37a273f4c09a6e1f6c1bf6fc8e6674cc49
kettek <kts@kettek.net>
2024-07-02 15:56:18 -0700

    Allow add existing palette color; show warning if palette doesn't exist
    Closes #18

--- frontend/src/components/ColorIndex.svelte
+++ frontend/src/components/ColorIndex.svelte
@@ -5,7 +5,7 @@
 -->
 <script lang='ts'>
   import { Button } from "carbon-components-svelte"
-  import { AddLarge, ColorSwitch } from "carbon-icons-svelte"
+  import { AddLarge, ColorSwitch, WarningAltFilled } from "carbon-icons-svelte"
   import { type LoadedFile } from "../types/file"
   import { ReplaceSwatchUndoable, AddSwatchUndoable } from "../types/file/undoables"
   import { createEventDispatcher } from "svelte"
@@ -67,9 +67,13 @@
         file.canvas.refreshImageData()
         file.canvas.refreshCanvas()
       }
+      $brushSettings.primaryIndex = palette.swatches.length - 1
     } else {
       file.push(new AddSwatchUndoable(red, green, blue, alpha))
+      $brushSettings.primaryIndex = file.canvas.palette.length - 1
     }
+    file = file
+    palette = palette
     dispatch('refresh', {})
     ;(document.activeElement as HTMLElement).blur()
   }
@@ -85,6 +89,8 @@
     } else {
       file.push(new ReplaceSwatchUndoable($brushSettings.primaryIndex, red, green, blue, alpha))
     }
+    file = file
+    palette = palette
     dispatch('refresh', {})
     ;(document.activeElement as HTMLElement).blur()
   }
@@ -101,10 +107,15 @@
         {/if}
       </span>
       <input type="text" style="color: rgb({255-red}, {255-green}, {255-blue})" value={hexCode} on:input={adjustHex}/>
+      {#if !swatchExists}
+        <span class='missing'>
+          <WarningAltFilled size={16} title='missing swatch' />
+        </span>
+      {/if}
     </div>
   </div>
-  <Button kind="ghost" size="small" disabled={swatchExists} iconDescription="replace swatch" tooltipPosition="top" icon={ColorSwitch} on:click={replaceSwatch}></Button>
-  <Button kind="ghost" size="small" disabled={swatchExists} iconDescription="add swatch" tooltipPosition="top" icon={AddLarge} on:click={addSwatch}></Button>
+  <Button kind="ghost" size="small" iconDescription="replace swatch" tooltipPosition="top" icon={ColorSwitch} on:click={replaceSwatch}></Button>
+  <Button kind="ghost" size="small" iconDescription="add swatch" tooltipPosition="top" icon={AddLarge} on:click={addSwatch}></Button>
 </main>
 
 <style>
@@ -135,5 +146,12 @@
     display: block;
     border: 0;
     font-family: 'Courier New', Courier, monospace;
+  }
+  .missing {
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding-right: 0.5rem;
+    padding-top: 0.5rem;
   }
 </style>
