2776fe7d678468f5cec2e82a648d9538925158db
kettek <kts@kettek.net>
2024-08-07 23:24:13 -0700

    Only get os separator once

--- frontend/src/sections/RichPresence.svelte
+++ frontend/src/sections/RichPresence.svelte
@@ -5,6 +5,7 @@
   import { LoadedFile } from '../types/file'
 
   export let is3D: boolean
+  let separator: string = '/'
 
   let lastFocusedFile: LoadedFile | null
   $: if (lastFocusedFile != $fileStates.focused) syncFile($fileStates.focused)
@@ -18,7 +19,7 @@
   async function refresh() {
     if (lastFocusedFile) {
       await SetRichPresenceState(`Editing in ${is3D? '3D' : '2D'}`)
-      await SetRichPresenceDetails(lastFocusedFile.filepath?lastFocusedFile.filepath.split((await GetOSSeparator() as string)).pop():'something special')
+      await SetRichPresenceDetails(lastFocusedFile.filepath?lastFocusedFile.filepath.split(separator).pop():'something special')
     } else {
       await SetRichPresenceState("Idle")
       await SetRichPresenceDetails("No file open")
@@ -27,6 +28,7 @@
   }
 
   onMount(async () => {
+    separator = await GetOSSeparator() as string
     await StartRichPresence()
     await SetRichPresenceState("Idle")
     await SetRichPresenceDetails("No file open")
