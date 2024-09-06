e7e85d368d6e8d16fb09f432f67e55b7af7b31c0
kettek <kts@kettek.net>
2024-09-05 22:14:37 -0700

    Implement capturing current view as image reference
    Closes #40

--- frontend/src/components/2d/imageReferenceTool.svelte
+++ frontend/src/components/2d/imageReferenceTool.svelte
@@ -5,7 +5,7 @@
   import { editor2DSettings } from '../../stores/editor2d'
   import { fileStates } from '../../stores/file'
   import type { LoadedFile } from '../../types/file'
-  import { TrashCan } from 'carbon-icons-svelte'
+  import { CameraAction, TrashCan } from 'carbon-icons-svelte'
   import Input from '../common/Input.svelte'
 
   export let file: LoadedFile
@@ -20,6 +20,27 @@
   function handleSelect(e: CustomEvent<{ selectedItem: { id: string; text: string } }>) {
     file.selectedImageReference = e.detail.selectedItem.id
     file.refresh()
+  }
+  function grabCurrentView() {
+    const img = file.view.toImage()
+    if (!img) return // TODO: alert error
+
+    imageReferences.add({
+      image: img,
+      src: `${file.filepath.slice(file.filepath.lastIndexOf('/') + 1)}.${file.stackName}.${file.animationName}.${file.frameIndex}.${file.sliceIndex}`,
+      x: 0,
+      y: 0,
+
+      flipX: false,
+      flipY: false,
+
+      rotation: 0,
+
+      zoom: 1,
+      opacity: 0.75,
+
+      overtop: false,
+    })
   }
   function removeSelected() {
     imageReferences.remove(file.selectedImageReference)
@@ -58,29 +79,38 @@
   }
 </script>
 
-<Dropdown size="sm" hideLabel selectedId={$file.selectedImageReference} {items} on:select={handleSelect} />
-{#if ref}
-  <Button size="small" kind="ghost" icon={TrashCan} on:click={removeSelected} />
-  <aside>
-    <Input type="number" width={4} size="small" value={ref.x} step={1} on:change={handleX} on:input={handleX}>
-      <svelte:fragment slot="label">x:</svelte:fragment>
-    </Input>
-    <Input type="number" width={4} size="small" value={ref.y} step={1} on:change={handleY} on:input={handleY}>
-      <svelte:fragment slot="label">y:</svelte:fragment>
-    </Input>
-    <Input type="number" width={4} size="small" value={ref.zoom} step={1} on:change={handleZoom} on:input={handleZoom}>
-      <svelte:fragment slot="label">zoom:</svelte:fragment>
-    </Input>
-    <Input type="number" width={4} size="small" step={0.05} value={ref.opacity} on:change={handleOpacity} on:input={handleOpacity}>
-      <svelte:fragment slot="label">opacity:</svelte:fragment>
-    </Input>
-    <Input type="checkbox" size="small" checked={ref.overtop} on:change={handleOvertop} on:input={handleOvertop}>
-      <svelte:fragment slot="label">overtop:</svelte:fragment>
-    </Input>
-  </aside>
-{/if}
+<menu>
+  <Dropdown size="sm" hideLabel selectedId={$file.selectedImageReference} {items} on:select={handleSelect} />
+  <Button size="small" kind="ghost" icon={CameraAction} on:click={grabCurrentView} tooltip="grab current view" tooltipPosition="bottom" />
+  {#if ref}
+    <Button size="small" kind="ghost" icon={TrashCan} on:click={removeSelected} />
+    <aside>
+      <Input type="number" width={4} size="small" value={ref.x} step={1} on:change={handleX} on:input={handleX}>
+        <svelte:fragment slot="label">x:</svelte:fragment>
+      </Input>
+      <Input type="number" width={4} size="small" value={ref.y} step={1} on:change={handleY} on:input={handleY}>
+        <svelte:fragment slot="label">y:</svelte:fragment>
+      </Input>
+      <Input type="number" width={4} size="small" value={ref.zoom} step={1} on:change={handleZoom} on:input={handleZoom}>
+        <svelte:fragment slot="label">zoom:</svelte:fragment>
+      </Input>
+      <Input type="number" width={5} size="small" step={0.05} value={ref.opacity} on:change={handleOpacity} on:input={handleOpacity}>
+        <svelte:fragment slot="label">opacity:</svelte:fragment>
+      </Input>
+      <Input type="checkbox" size="small" checked={ref.overtop} on:change={handleOvertop} on:input={handleOvertop}>
+        <svelte:fragment slot="label">overtop:</svelte:fragment>
+      </Input>
+    </aside>
+  {/if}
+</menu>
 
 <style>
+  menu {
+    display: grid;
+    grid-template-columns: auto auto auto minmax(0, 1fr);
+    grid-template-rows: minmax(0, 1fr);
+    gap: 0.5rem;
+  }
   input {
     width: 3rem;
   }
