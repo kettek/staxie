88f70ff0ef2e2f30add77853a8bd354077bf3e19
kettek <kts@kettek.net>
2024-09-17 23:50:52 -0700

    Stub out a lil more

--- frontend/src/components/3d/OptimizedRender.svelte
+++ frontend/src/components/3d/OptimizedRender.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
   import type { LoadedFile } from 'src/types/file'
+  import { onMount } from 'svelte'
 
   export let file: LoadedFile
+  $: file ? refreshMesh() : null
+
+  function refreshMesh() {
+    // TODO: Here we would run a greedy mesher against our slices.
+  }
+
+  onMount(() => {
+    refreshMesh()
+  })
 </script>
