3ff90d803ac06843e4f93f8aef8af9c074dc1442
kettek <kts@kettek.net>
2024-10-23 15:58:25 -0700

    Allow specifying origin on rotation

--- frontend/src/toolbars/Selection3D.svelte
+++ frontend/src/toolbars/Selection3D.svelte
@@ -11,6 +11,9 @@
   let radX: number = 0
   let radY: number = 0
   let radZ: number = 0
+  let originX: number | string = ''
+  let originY: number | string = ''
+  let originZ: number | string = ''
   $: radX = rotateX * (Math.PI / 180)
   $: radY = rotateY * (Math.PI / 180)
   $: radZ = rotateZ * (Math.PI / 180)
@@ -34,7 +37,8 @@
     const maxZ = Math.max(c1[2], c2[2])
 
     const size = [maxX - minX, maxY - minY, maxZ - minZ]
-    const center = [Math.floor(minX + size[0] / 2), Math.floor(minY + size[1] / 2), Math.floor(minZ + size[2] / 2)]
+    let center = [Math.floor(minX + size[0] / 2), Math.floor(minY + size[1] / 2), Math.floor(minZ + size[2] / 2)]
+    center = [Math.round(originX === '' ? center[0] : parseFloat(originX as string)), Math.round(originY === '' ? center[1] : parseFloat(originY as string)), Math.round(originZ === '' ? center[2] : parseFloat(originZ as string))]
 
     const oldPixels: { x: number; y: number; index: number }[] = []
     const newPixels: { x: number; y: number; index: number }[] = []
@@ -62,7 +66,7 @@
 
           const nslice = $fileStates.focused.frame?.slices[y4]
           if (!nslice) continue
-          if (nslice.x + x4 < nslice.x || nslice.x + x4 >= nslice.x+$fileStates.focused.frameWidth || nslice.y + z4 < nslice.y || nslice.y + z4 >= nslice.y+$fileStates.focused.frameHeight) continue
+          if (nslice.x + x4 < nslice.x || nslice.x + x4 >= nslice.x + $fileStates.focused.frameWidth || nslice.y + z4 < nslice.y || nslice.y + z4 >= nslice.y + $fileStates.focused.frameHeight) continue
           newPixels.push({ x: nslice.x + x4, y: nslice.y + z4, index: p })
         }
       }
@@ -73,14 +77,24 @@
 
 <OverflowMenu size="sm">
   <div slot="menu">Rotate</div>
-  <Input type="number" bind:value={rotateX} labelColor='#ff3553'>
+  <Input type="number" bind:value={rotateX} labelColor="#ff3553">
     <svelte:fragment slot="label">X</svelte:fragment>
   </Input>
-  <Input type="number" bind:value={rotateY} labelColor='#8adb00'>
+  <Input type="number" bind:value={rotateY} labelColor="#8adb00">
     <svelte:fragment slot="label">Y</svelte:fragment>
   </Input>
-  <Input type="number" bind:value={rotateZ} labelColor='#2a8fff'>
+  <Input type="number" bind:value={rotateZ} labelColor="#2a8fff">
     <svelte:fragment slot="label">Z</svelte:fragment>
+  </Input>
+  <hr />
+  <Input type="number" bind:value={originX} labelColor="#ff3553" placeholder="center">
+    <svelte:fragment slot="label">Origin X</svelte:fragment>
+  </Input>
+  <Input type="number" bind:value={originY} labelColor="#8adb00" placeholder="center">
+    <svelte:fragment slot="label">Origin Y</svelte:fragment>
+  </Input>
+  <Input type="number" bind:value={originZ} labelColor="#2a8fff" placeholder="center">
+    <svelte:fragment slot="label">Origin Z</svelte:fragment>
   </Input>
   <Button kind="primary" on:click={doRotate}>Apply</Button>
 </OverflowMenu>
